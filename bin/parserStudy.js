b2f393edb759f65415193cadf86afaeeba7a969b
John Caughman <caughmanjb1@ornl.gov>
2014-06-27 13:39:01 -0400

    finished studyMetadata parser

--- bin/parserStudy.js
+++ bin/parserStudy.js
@@ -3,13 +3,64 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
 var studyMetadata = require('../models/studyMetadata.js');
+
+var header = [
+		'studyName',
+		'studyId',
+		'studyGoldId',
+		'studyLegacyGoldId',
+		'ncbiId',
+		'ncbiAccension',
+		'ncbiName',
+		'sequencingCenter',
+		'funding',
+		'contact',
+		'email',
+		'published',
+		'availability'
+		//'submissionDate'
+];
+
+
+var saveArray = [];
 var csv = require('csv');
 csv()
-.fromPath(__dirname+ '/filename.csv', {delimiter: '\t'})
-toPath(__dirname+'filename.out');
+.fromPath('../data/bacterialStudy.csv', {delimiter: '\t'})
 .transform(function(row){
 	return row;
-});
+})
 .on('data', function(row, index2){
+	newRow = new studyMetadata();
+	newObj = lodash.zipObject(header, row);
+	lodash.assign(newRow, newObj);
+	saveArray.push(newRow);
+})
+.on('end', function(count){
+	saveArray.forEach(function(item, index){
+		if(index +1 == saveArray.length){
+			item.save(function(err, item){
+				if(err){
+					console.log(err);
+					process.exit(0);
+				} else{
+					console.log(item);
+					process.exit(0);
+				}
+			});
+		} else {
+			item.save(function(err, item){
+				if(err){
+					console.log(err);
+				} else {
+					console.log(item);
+				}
+			});
+		}
+	});
+
+	console.log('Number of lines: ' + count);
 
 })
+.on('error', function(error){
+	console.log(error.message);
+});
--- models/studyMetadata.js
+++ models/studyMetadata.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
    ,Schema= mongoose.Schema;
-
+var validate = require('mongoose-validate');
 function validateEmail(email){
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
@@ -19,7 +19,7 @@ var studyMetadataSchema = new Schema({
 	sequencingCenter: {type: String, default: null},
 	funding: {type: String, default: null},
 	contact: {type: String, default: null, required: true},
-	email: {type: String, default: null validate: [validateEmail, 'email not valid'], required: true },
+	email: {type: String, default: null, validate: [validate.email, 'email not valid'], required: true },
 	published: {type: String, default: null},
 	availability: {type: String, default: null, required: true},
 	submissionDate: {type: Date, default: Date.now}
